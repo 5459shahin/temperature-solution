b84105af70cb35ccdaf2c50e9307b246d2ff648c
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-03-04 02:25:50 +0800

    first commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 const API_KEY = `8c6d72c6bf0eead6653519b7a0842a67`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-    // console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    console.log(url);
     fetch(url)
     .then(res => res.json())
     .then(data => displayTemperature(data))
 };
 
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
 const displayTemperature = temperature =>{
-    console.log(temperature)
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp)
+    setInnerText('condition', temperature.weather[0].main)
+    console.log(temperature);
+    //set weather icon
+    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
 
 }
 
